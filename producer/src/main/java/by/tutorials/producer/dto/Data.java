410e07936afd42d54a9e470a27dc1961a076f334
Дробыш Артём Николаевич <artsiom.drobysh@softclub.by>
2021-09-27 18:02:30 +0300

    Init.
    Add producer.

---
+++ producer/src/main/java/by/tutorials/producer/dto/Data.java
@@ -0,0 +1,4 @@
+package by.tutorials.producer.dto;
+
+public record Data(String message) {
+}
---
+++ producer/src/main/java/by/tutorials/producer/web/DataController.java
@@ -0,0 +1,28 @@
+package by.tutorials.producer.web;
+
+import by.tutorials.producer.dto.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DataController {
+    
+    @Autowired
+    private KafkaTemplate<String, Data> dataKafkaTemplate;
+    @Value("${app.kafka.topics.data}")
+    private String dataTopic; 
+    
+    @PostMapping
+    public void sendData(@RequestBody Data data) {
+        dataKafkaTemplate.send(dataTopic, data).addCallback(
+            sr -> log.info("SUCCESS : {}", sr),
+            t -> log.error("ERROR", t)
+        );
+    }
+}
